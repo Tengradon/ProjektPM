b0e5baa3c94ce7eca06b29c64c353dffba68ec17
connors578 <connors578@interia.pl>
2016-06-27 22:31:23 +0200

    klasa Results

---
+++ app/src/main/java/com/example/tengradon/asystentgierlosowych/Results.java
@@ -0,0 +1,59 @@
+package com.example.tengradon.asystentgierlosowych;
+
+import java.util.Date;
+
+/**
+ * Created by Tengradon on 2016-06-27.
+ */
+public class Results {
+
+    private int id;
+    private TypGry typGry;
+    private Date dataLosowania;
+    private String wylosowaneLiczby;
+
+    public Results(int id, TypGry typGry, Date dataLosowania, String wylosowaneLiczby) {
+        this.id = id;
+        this.typGry = typGry;
+        this.dataLosowania = dataLosowania;
+        this.wylosowaneLiczby = wylosowaneLiczby;
+    }
+
+    public Results(TypGry typGry, Date dataLosowania, String wylosowaneLiczby) {
+        this.typGry = typGry;
+        this.dataLosowania = dataLosowania;
+        this.wylosowaneLiczby = wylosowaneLiczby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TypGry getTypGry() {
+        return typGry;
+    }
+
+    public void setTypGry(TypGry typGry) {
+        this.typGry = typGry;
+    }
+
+    public Date getDataLosowania() {
+        return dataLosowania;
+    }
+
+    public void setDataLosowania(Date dataLosowania) {
+        this.dataLosowania = dataLosowania;
+    }
+
+    public String getWylosowaneLiczby() {
+        return wylosowaneLiczby;
+    }
+
+    public void setWylosowaneLiczby(String wylosowaneLiczby) {
+        this.wylosowaneLiczby = wylosowaneLiczby;
+    }
+}
