f4c3af32dc6c8b04d4981ebefabe05f0c8079a78
Rafał Kusz <ravqsz14@gmail.com>
2016-06-28 10:21:08 +0200

    zmiany w klasie typGry

--- app/src/main/java/com/example/tengradon/asystentgierlosowych/TypGry.java
+++ app/src/main/java/com/example/tengradon/asystentgierlosowych/TypGry.java
@@ -4,5 +4,12 @@ package com.example.tengradon.asystentgierlosowych;
  * Created by Rafal on 2016-06-27.
  */
 public enum TypGry {
-    LOTTO, EKSTRA_PENSJA, MULTI_MULTI14, MULTI_MULTI22, MINI_LOTTO
+    LOTTO(1), EKSTRA_PENSJA(2), MULTI_MULTI14(3), MULTI_MULTI22(4), MINI_LOTTO(5);
+    private final int value;
+    TypGry(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
 }
