008570130ba5eb6a2dfbef25d179287bfec48fc2
Rafał Kusz <ravqsz14@gmail.com>
2016-06-28 09:59:42 +0200

    dodane sortowanie liczb i pobieranie wynikow z serwera - klasa obrabianie wynikow

--- app/src/main/java/com/example/tengradon/asystentgierlosowych/TypGry.java
+++ app/src/main/java/com/example/tengradon/asystentgierlosowych/TypGry.java
@@ -4,5 +4,5 @@ package com.example.tengradon.asystentgierlosowych;
  * Created by Rafal on 2016-06-27.
  */
 public enum TypGry {
-    LOTTO, EKSTRA_PENSJA, MULTI_MULTI, MINI_LOTTO, KASKADA, SUPER_SZANSA
+    LOTTO, EKSTRA_PENSJA, MULTI_MULTI14, MULTI_MULTI22, MINI_LOTTO
 }
