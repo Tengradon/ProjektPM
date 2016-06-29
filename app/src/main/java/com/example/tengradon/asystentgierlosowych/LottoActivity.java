61001b953cff5dcbfbea3f0b426558c9845a977a
Rafał Kusz <ravqsz14@gmail.com>
2016-06-29 13:01:26 +0200

    dodanie aktywnosci LottoActivity

---
+++ app/src/main/java/com/example/tengradon/asystentgierlosowych/LottoActivity.java
@@ -0,0 +1,21 @@
+package com.example.tengradon.asystentgierlosowych;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Tengradon on 2016-06-29.
+ */
+public class LottoActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lotto_activity);
+    }
+
+    public void saveLottoNumbers(View view){
+
+    }
+}
